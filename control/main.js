5b48a29aeaf13cbc09339b05ae70209080ca5393
Josh (on Pi) <josh.curry@talktalk.net>
2017-02-10 02:06:48 +0000

    needs serial improvement

--- control/main.js
+++ control/main.js
@@ -17,7 +17,8 @@ function pad(num, size) {
 var atem = new ATEM();
  
 atem.on('connect', function() {
-	setInterval(state2displays, 40); 
+	setInterval(state2displays, 10); 
+	setInterval(flash, 1000);
 });
 
 port.on('open', function() {
@@ -25,12 +26,58 @@ port.on('open', function() {
     atem.connect('10.0.1.220')  
 });
 
+var laststate;
+var flashs, flashing;
+
+function flash() {
+	if(flashing) {
+	if(flashs==1) {
+	flashs = 0;
+	port.write("l162,1,"+Buffer.from("00", "hex")); 
+	}
+	else {
+	flashs = 1;
+	port.write("l162,0,"+Buffer.from("00", "hex")); 
+	}
+	}
+	else {
+port.write("l162,1,"+Buffer.from("00", "hex"));
+port.write("l163,1,"+Buffer.from("00", "hex"));
+}
+}
+
+var prevprog, prevprev;
+
 function state2displays() {
 	state = atem.state.video.ME[0].transitionFrameCount;
-	if(state!=undefined) {
+	pos = atem.state.video.ME[0].transitionPosition;
+	if(pos>0) {
+		flashing = 1;
+	}
+	else {
+	flashing = 0;
+	}
+	if(state!=undefined && state!=laststate) {
 		padded=pad(state,3);
+		laststate = state;
 		port.write("d2,"+padded+Buffer.from("00", "hex"));
 	}
+
+	prog = atem.state.video.ME[0].programInput;
+	prev = atem.state.video.ME[0].previewInput;
+	
+	if(prog!=prevprog) {
+		port.write("l,"+(prevprog+19)+Buffer.from("00", "hex"));
+		port.write("l,"+(prog+19)+",1"+Buffer.from("00", "hex")); 
+	} 
+
+        if(prev!=prevprev) {
+                port.write("l,"+(prevprev+39)+",0"+Buffer.from("00", "hex"));
+                port.write("l,"+(prev+39)+",1"+Buffer.from("00", "hex"));
+        }
+
+	prevprog = prog;
+	prevprev = prev;
 }
 
 
@@ -62,6 +109,16 @@ if(cmd=="b") {
 
 
 function parseButton(uid) {
+
+	if(40>uid>=20) {
+		console.log("PROG_CHANGE");
+		atem.changeProgramInput(uid-19);
+	}
+
+	if(60>uid>=40) {
+		console.log("PREV CHANGE");
+		atem.changePreviewInput(uid-39);  
+	}
 
 	switch(uid) {
 		case 162:
